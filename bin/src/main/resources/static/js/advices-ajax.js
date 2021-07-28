3f54b89eec29a1cb90d915fbfabb2d212ba29b8b
Giuseppe <mazzeogiu@gmail.com>
2021-07-28 11:11:05 +0200

    bin

---
+++ bin/src/main/resources/static/js/advices-ajax.js
@@ -0,0 +1,35 @@
+function doAdvices(productname) {
+
+	//var cheeseSelected = document.getElementById("txtCheeseSelected").value;
+	//var url = "http://localhost:8989/AdviceRest/advice-api-rest/advices/"+cheeseSelected;
+	var url = "http://localhost:8989/AdviceRest/advice-api-rest/advices/"+productname;
+
+	//Conversion et affichage
+	var callback = function(data) {
+		console.log("success data=" + data);
+		var dataAdvices = JSON.parse(data);
+		console.log(dataAdvices);
+		console.log(dataAdvices[0].category);
+
+		var html = "";
+		for (let i = 0; i < dataAdvices.length; i++) {
+			html += "<td>" + dataAdvices[i].drinkName + "</td>"
+				+ "</td><td>" + dataAdvices[i].drinkType + "</td></tr>";
+		}
+
+		document.getElementById("data").innerHTML = html;
+	}
+
+	//erreur
+	var errCallback = function(data) {
+		console.log("erreur=" + data);
+	}
+
+	makeAjaxGetRequest(url, callback, errCallback);
+
+}
+
+
+
+//code
+//doAdvices('roquefort');
